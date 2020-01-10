181b72169657ef0b01ae7ddfcda2d9e7e1e83a6f
renzhongrui <zrzoffice@163.com>
2020-01-10 15:10:19 +0800

    添加插件工程启动工程

---
+++ 004-android-hook-activity/PluginApp/src/main/java/com/learn/hook/plugin/Test.java
@@ -0,0 +1,9 @@
+package com.learn.hook.plugin;
+
+public class Test {
+
+    public static void test() {
+        System.out.println("test");
+    }
+
+}
--- 004-android-hook-activity/app/src/main/java/com/learn/hook/activity/MainActivity.java
+++ 004-android-hook-activity/app/src/main/java/com/learn/hook/activity/MainActivity.java
@@ -2,12 +2,40 @@ package com.learn.hook.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //printClassLoader();
+        invokePluginTest();
     }
+
+    // 打印classloader
+    private void printClassLoader() {
+        ClassLoader classLoader = getClassLoader();
+        while (classLoader != null) {
+            Log.e(TAG, "printClassLoader: " + classLoader);
+            classLoader = classLoader.getParent();
+        }
+    }
+
+    private void invokePluginTest() {
+        try {
+            Class<?> clazz = Class.forName("com.learn.hook.plugin.Test");
+            Method test = clazz.getMethod("test");
+            test.invoke(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
