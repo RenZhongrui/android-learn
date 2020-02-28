cd95dee294dab2bbe7fb47ec7f530358f50e9cf2
renzhongrui <zrzoffice@163.com>
2020-02-28 15:51:50 +0800

    引入资源，编写上传maven私服配置

---
+++ 006_android_music/app_music/src/main/java/com/learn/music/views/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.learn.music.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.learn.music.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+
+}
