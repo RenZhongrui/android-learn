f41f4f02556cfefe3374673b668eafa4e5a9b63d
renzhongrui <zrzoffice@163.com>
2020-02-28 16:47:58 +0800

    添加network模块

---
+++ 006_android_music/lib_network/src/main/java/com/lib/network/CommonOkHttpClient.java
@@ -0,0 +1,89 @@
+package com.lib.network;
+
+
+import com.lib.network.cookie.SimpleCookieJar;
+import com.lib.network.https.HttpsUtils;
+import com.lib.network.listener.DisposeDataHandle;
+import com.lib.network.response.CommonFileCallback;
+import com.lib.network.response.CommonJsonCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.Call;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @author qndroid
+ * @function 用来发送get, post请求的工具类，包括设置一些请求的共用参数
+ */
+public class CommonOkHttpClient {
+    private static final int TIME_OUT = 30;
+    private static OkHttpClient mOkHttpClient;
+
+    static {
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        okHttpClientBuilder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        /**
+         *  为所有请求添加请求头，看个人需求
+         */
+        okHttpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request =
+                        chain.request().newBuilder().addHeader("User-Agent", "Imooc-Mobile") // 标明发送本次请求的客户端
+                                .build();
+                return chain.proceed(request);
+            }
+        });
+        okHttpClientBuilder.cookieJar(new SimpleCookieJar());
+        okHttpClientBuilder.connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpClientBuilder.readTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpClientBuilder.writeTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpClientBuilder.followRedirects(true);
+        /**
+         * trust all the https point
+         */
+        okHttpClientBuilder.sslSocketFactory(HttpsUtils.initSSLSocketFactory(),
+                HttpsUtils.initTrustManager());
+        mOkHttpClient = okHttpClientBuilder.build();
+    }
+
+    public static OkHttpClient getOkHttpClient() {
+        return mOkHttpClient;
+    }
+
+
+    /**
+     * 通过构造好的Request,Callback去发送请求
+     */
+    public static Call get(Request request, DisposeDataHandle handle) {
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonJsonCallback(handle));
+        return call;
+    }
+
+    public static Call post(Request request, DisposeDataHandle handle) {
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonJsonCallback(handle));
+        return call;
+    }
+
+    public static Call downloadFile(Request request, DisposeDataHandle handle) {
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new CommonFileCallback(handle));
+        return call;
+    }
+}
