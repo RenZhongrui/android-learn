79e77f7bcfd1c7c1ff04428d95a03eaf840e7683
renzhongrui <zrzoffice@163.com>
2020-03-15 12:49:04 +0800

    添加扩展属性

--- 007_android_gradle_plugin_buildSrc/buildSrc/src/main/java/com.learn.plugin/MyPlugin.java
+++ 007_android_gradle_plugin_buildSrc/buildSrc/src/main/java/com.learn.plugin/MyPlugin.java
@@ -1,5 +1,6 @@
 package com.learn.plugin;
 
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -9,5 +10,16 @@ public class MyPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         System.out.println("11111111111111");
+        // 创建扩展
+        Param param = project.getExtensions().create("testGradlePlugin", Param.class);
+        // gradle build完之后可以获取配置的参数value
+        project.afterEvaluate(new Action<Project>() {
+            @Override
+            public void execute(Project project) {
+                System.out.println(param.getVersion());
+                System.out.println(param.getName());
+            }
+        });
+
     }
 }
---
+++ 007_android_gradle_plugin_buildSrc/buildSrc/src/main/java/com.learn.plugin/Param.java
@@ -0,0 +1,29 @@
+package com.learn.plugin;
+
+/**
+ * create: Ren Zhongrui
+ * date: 2020-03-15
+ * description: 创建bean实体来存储扩展参数
+ */
+public class Param {
+
+    private int version;
+    private String name;
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
