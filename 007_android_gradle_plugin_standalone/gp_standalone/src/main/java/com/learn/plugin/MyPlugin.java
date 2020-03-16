308e4440fda5b6ddfd2d206ee22e918719de7f6a
renzhongrui <zrzoffice@163.com>
2020-03-17 00:17:22 +0800

    自定义gradle插件并上传maven私服

---
+++ 007_android_gradle_plugin_standalone/gp_standalone/src/main/java/com/learn/plugin/MyPlugin.java
@@ -0,0 +1,14 @@
+package com.learn.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MyPlugin implements Plugin<Project> {
+
+
+    @Override
+    public void apply(Project project) {
+        System.out.println("11111111111111");
+
+    }
+}
---
+++ 008_android_ams/app/src/main/java/com/learn/ams/Test.java
@@ -0,0 +1,13 @@
+package com.learn.ams;
+
+/**
+ * create: Ren Zhongrui
+ * date: 2020-03-16
+ * description:
+ */
+public class Test {
+
+    public void test() {
+
+    }
+}
