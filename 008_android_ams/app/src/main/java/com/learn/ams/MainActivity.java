9f4381e3877cf61896df752362d0de19c7d81a1f
renzhongrui <zrzoffice@163.com>
2020-03-17 09:30:34 +0800

    自定义gradle插件并上传maven私服

--- 008_android_ams/app/src/main/java/com/learn/ams/MainActivity.java
+++ 008_android_ams/app/src/main/java/com/learn/ams/MainActivity.java
@@ -1,14 +1,25 @@
 package com.learn.ams;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    private Button btn_to;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_to = findViewById(R.id.btn_to);
+        btn_to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
