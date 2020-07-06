7cc75433ad658fe77529c396d453eacd20acd0bd
renzhongrui <zrzoffice@163.com>
2020-07-06 10:11:33 +0800

    wcdb数据库的使用

--- 002-android-room/app/src/main/java/com/learn/room/entity/Book.java
+++ 002-android-room/app/src/main/java/com/learn/room/entity/Book.java
@@ -4,7 +4,6 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
-import android.os.Build;
 
 @Entity(tableName = "book")
 public class Book {
---
+++ 002-android-wcdb/app/src/main/java/com/learn/wcdb/DBUtil.java
@@ -0,0 +1,14 @@
+package com.learn.wcdb;
+
+/**
+ *  WCDB工具类封装
+ *  接口文档：https://tencent.github.io/wcdb/references/android/reference/packages.html
+ *  使用示例 https://www.cnblogs.com/zdz8207/p/android-local-wcdb.html
+ *  使用注意事项：https://blog.csdn.net/okgays/article/details/86603565
+ *  工具：https://blog.csdn.net/qiantanlong/article/details/79456235?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-4.nonecase
+ */
+public class DBUtil {
+
+
+
+}
