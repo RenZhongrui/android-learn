8586998ce9b0f24abd16f2ebe15784142a7d4a1b
renzhongrui <zrzoffice@163.com>
2020-08-07 15:28:43 +0800

    学习thread handler 循环队列的使用

--- 017-android-thread-handler/app/src/main/java/com/learn/thread/handler/MainActivity.java
+++ 017-android-thread-handler/app/src/main/java/com/learn/thread/handler/MainActivity.java
@@ -12,6 +12,11 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MSG_CPU = 1;
+    /**
+     * 信息采集时间 内存和cpu
+     */
+    private static final int NORMAL_SAMPLING_TIME = 500;
 
     private HandlerThread mHandlerThread;
     /**
@@ -34,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void handleMessage(Message msg) {
                     super.handleMessage(msg);
-                    Log.e(TAG, "handleMessage: ");
-
+                    if (msg.what == MSG_CPU) {
+                        Log.e(TAG, "handleMessage: MSG_CPU");
+                        workHandler.sendEmptyMessageDelayed(MSG_CPU, NORMAL_SAMPLING_TIME);
+                    }
                 }
             };
         }
@@ -45,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // a. 定义要发送的消息
                 Message msg = Message.obtain();
-                msg.what = 2; //消息的标识
+                msg.what = MSG_CPU; //消息的标识
                 msg.obj = "B"; // 消息的存放
                 // b. 通过Handler发送消息到其绑定的消息队列
                 workHandler.sendMessage(msg);
